59723d35ab99c88ac6de4f23d2411bf1348cf38b
Ali Ghani <ali48pk@hotmail.com>
2025-08-26 03:13:31 +0500

    fix for ios 14 banner

--- ios/Classes/LocationManager.swift
+++ ios/Classes/LocationManager.swift
@@ -475,6 +475,10 @@ extension LocationManager: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound, .badge])
+        if #available(iOS 14.0, *) {
+            completionHandler([.banner, .sound, .badge])
+        } else {
+            completionHandler([.alert, .sound, .badge])
+        }
     }
 }
